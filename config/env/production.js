bffc1805eea81a0912f1434c806088445ee5e37d
Npitcher12 <63477243+Npitcher12@users.noreply.github.com>
2023-03-10 14:36:25 -0500

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -148,9 +148,9 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     cors: {
-      // allowOrigins: [
-      //   'https://example.com',
-      // ]
+      allowOrigins: [
+        'https://novoland.osc-fr1.scalingo.io',
+      ]
     },
 
   },
